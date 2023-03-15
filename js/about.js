c1cf51348f9a11b77e6cf89ba3a97dae55a75fa9
leesuenghee <95907364+leesuenghee@users.noreply.github.com>
2023-03-15 15:05:40 +0900

    about_section 수정

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
 let fonts = $(window).scrollTop();
 console.log(fonts);
-let rowSlide = $('#rowSlide').scrollTop();
+let rowSlide = $("#rowSlide").scrollTop();
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.to("#fonts > div .text_1", {
@@ -34,89 +34,25 @@ gsap.to("#fonts > div .text_3", {
   duration: 3,
 });
 
-// slide_03
-// gsap.registerPlugin(ScrollTrigger);
-
-// gsap.set(".rowFlex img", { zIndex: (i, target, targets) => targets.length - i });
-
-// var images = gsap.utils.toArray('.rowFlex img');
-
-// images.forEach((image, i) => {
-  
-//   var tl = gsap.timeline({
-    
-//     scrollTrigger: {
-//       trigger: "#rowSlide",
-//       start: () => "top -" + (window.innerHeight*(i+0.5)),
-//       end: () => "+=" + window.innerHeight,
-//       scrub: true,
-//       toggleActions: "play none reverse none",
-//       invalidateOnRefresh: true,     
-//     }
-    
-//   })
-  
-//   tl
-//   .to(image, { height: 912 })
-//   ;
-  
-// });
-
-// gsap.set(".rowFlex span", { zIndex: (i, target, targets) => targets.length - i });
-
-// var texts = gsap.utils.toArray('.rowFlex span');
-
-// texts.forEach((text, i) => {
-  
-//   var tl = gsap.timeline({
-    
-//     scrollTrigger: {
-//       trigger: "#rowSlide",
-//       start: () => "top -" + (window.innerHeight*i),
-//       end: () => "+=" + window.innerHeight,
-//       scrub: true,
-//       toggleActions: "play none reverse none",
-//       invalidateOnRefresh: true,     
-//     }
-    
-//   })
-  
-//   tl
-//   .to(text, { duration: 0.33, opacity: 1, y:"50%" })  
-//   .to(text, { duration: 0.33, opacity: 0, y:"50%" })
-//   ;
-  
-// });
-// ScrollTrigger.create({
-
-//   trigger: "#rowSlide",
-//   scrub: true,
-//   markers: true,
-//   pin: true,
-//   start: () => "top top",
-//   end: () => "+=" + ((images.length + 1) * window.innerHeight),
-//   invalidateOnRefresh: true,
-
-// });
-// gsap.registerPlugin(ScrollTrigger);
-// gsap.utils.toArray("#rowSlide").forEach((section, i) => {
-//   ScrollTrigger.create({
-//     trigger: section,
-//     // start: "top 0%", 
-//     pin: true,
-//     pinSpacing: false,
-//   });
-// });
-// ScrollTrigger.create({
-//   snap: 1 / 2
-// });
-
-gsap.to('#rowSlide .box',{
+// section_03
+gsap.to("#rowSlide .box", {
   scrollTrigger: {
-      trigger: '#rowSlide .box',
-      start: 'top 0%',
-      endTrigger: '#rowSlide',
-      scrub: 1,
-      pin: true
-  }
-})
+    trigger: "#rowSlide .box",
+    start: "top 0%",
+    endTrigger: "#rowSlide",
+    scrub: 1,
+    pin: true,
+  },
+});
+
+//slide
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
